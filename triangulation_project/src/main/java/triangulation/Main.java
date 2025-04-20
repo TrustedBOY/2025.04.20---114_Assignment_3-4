0054ee3cd5a38e90d50657e3f04005298a5ff18e
TrustedBOY <amirhoseinsaebi.83@gmail.com>
2025-04-20 15:56:22 +0300

    Test Case For Area Added

--- triangulation_project/src/main/java/triangulation/Main.java
+++ triangulation_project/src/main/java/triangulation/Main.java
@@ -2,18 +2,20 @@ package triangulation;
 
 public class Main {
     public static void main(String[] args) {
+        // Test the Polygon class
+        testPolygonArea();
+
+        
+    }
+    private static void testPolygonArea() {
         Point[] points = {
-            new Point(1, 1),
-            new Point(4, 1),
-            new Point(3.5, 2),
+            new Point(0, 0),
+            new Point(4, 0),
             new Point(4, 3),
-            new Point(3, 2),
-            new Point(2, 3)
+            new Point(0, 3)
         };
         Polygon polygon = new Polygon(points);
-
-        System.out.println(polygon.area());
-
-        System.out.println(("check"));
+        double area = polygon.area();
+        System.out.println("Area of the polygon: " + area);
     }
 }
