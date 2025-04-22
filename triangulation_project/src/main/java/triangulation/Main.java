219ffb1e6366c65dd66fcfeaae53a0bd51d91e6d
TrustedBOY <amirhoseinsaebi.83@gmail.com>
2025-04-22 15:38:07 +0300

    Project is revived

--- triangulation_project/src/main/java/triangulation/Main.java
+++ triangulation_project/src/main/java/triangulation/Main.java
@@ -1,15 +1,17 @@
 package triangulation;
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<List<Point>> polygons = read();
-        ModelWriter pngWriter = new PNGWriter();
+
+        String readFilePath = "triangulation_project\\src\\inputData\\";
         String writeFilePath = "triangulation_project\\src\\outputImages\\";
+        ModelWriter pngWriter = new PNGWriter();
+        TextReader textReader = new TextReader(readFilePath);
+
+        List<List<Point>> polygons = textReader.getPolygons();
 
         for (int i = 0; i < polygons.size(); i++) {
 
@@ -18,27 +20,5 @@ public class Main {
         }
 
         System.out.println(polygons.size() + " polygons written to PNG files.");
-    }
-    private static List<List<Point>> read() {
-        String filePath = "triangulation_project\\src\\inputData\\";
-        List<List<Point>> polygons = new ArrayList<>();
-
-        for (int i = 0; true ; i++) {
-            String currentFilePath = filePath + (i + 1) + ".txt";
-            File file = new File(currentFilePath);
-
-            if (!file.exists()) {
-                break;
-            }
-
-            try {
-                polygons.add(TextReader.readVerticesFromFile(currentFilePath));
-            } catch (Exception e) {
-                System.err.println("Error reading file: " + e.getMessage());
-                break;
-            }
-        }
-
-        return polygons;
     }
 }
